051b08fd69cd7c94ccac25e149763bd794899f01
Clovis Fritzen <clovisf@gmail.com>
2025-03-03 21:12:09 -0300

    removed sleep function

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
 from machine import Pin
-from time import sleep
+import time
 import dht
 
 sensor = dht.DHT11(Pin(16))
+initialtime= time.ticks_ms() #https://docs.micropython.org/en/latest/library/time.html
 
 while True:
     try:
-        sleep(1)
-        # The DHT11 returns at most one measurement every 1s
-        sensor.measure()
-        # Retrieves measurements from the sensor
-        print(f"Temperature : {sensor.temperature():.1f}")
-        print(f"Humidity    : {sensor.humidity():.1f}")
-        # Transmits the temperature to the computer console
+        currenttime= time.ticks_ms() #Every time it passes here, gets the current time
+        if time.ticks_diff(time.ticks_ms(), initialtime) > 2000: # this IF will be true every 200 ms
+            initialtime= time.ticks_ms() #update with the "current" time
+
+            # The DHT11 returns at most one measurement every 1s
+            sensor.measure()
+            # Retrieves measurements from the sensor
+            print(f"Temperature : {sensor.temperature():.1f}")
+            print(f"Humidity    : {sensor.humidity():.1f}")
+            # Transmits the temperature to the computer console
     except OSError as e:
         print('Failed reception')
         # If esp does not receive the measurements from the sensor
